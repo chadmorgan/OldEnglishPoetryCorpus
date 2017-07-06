0a990b89ba81e722a0b79df7779a8cabe76b03c9
chadmorgan <chadallan.morgan@gmail.com>
2017-07-05 19:30:07 -0700

    labyrinth library poems scraped

--- scrape_poetry_corpus.py
+++ scrape_poetry_corpus.py
@@ -1,40 +1,71 @@
 import requests
 from bs4 import BeautifulSoup
 
-def clean_poetic_stanza(poetic_line):
-	poetic_line = str.lower(poetic_line)
+# -----------------------------------------------------------------------------
+# Scrape Old English poems from the corpus made available 
+# by Murray McGillivray from the University of Calgary English department
+# -----------------------------------------------------------------------------
+
+def clean_poetic_stanza(poetic_stanza):
+	poetic_stanza = str.lower(poetic_stanza)
 	# normalize: v -> u, k -> c
-	poetic_line = re.sub('v', 'u', poetic_line)
-	poetic_line = re.sub('k', 'c', poetic_line)
+	poetic_stanza = re.sub('v', 'u', poetic_stanza)
+	poetic_stanza = re.sub('k', 'c', poetic_stanza)
 	# remove anything not in the OE character set
-	poetic_line = re.sub('[^a-yþæð\s]', '', poetic_line)
-	poetic_line = re.sub(' +', ' ', poetic_line) 
-	poetic_line = re.sub('\xa0', '', poetic_line) 
-	return(poetic_line)
+	poetic_stanza = re.sub('[^a-yþæð\s]', '', poetic_stanza)
+	# remove \xa0
+	poetic_stanza = re.sub('\xa0', '', poetic_stanza) 
+	# remove extra spaces
+	poetic_stanza = re.sub(' +', ' ', poetic_stanza) 
+	# remove initial line break
+	poetic_stanza = re.sub('^\n', '', poetic_stanza) 
+	# remove final line break or spaces
+	poetic_stanza = re.sub('\n$', '', poetic_stanza) 
+	poetic_stanza = re.sub(' $', '', poetic_stanza) 
+	return(poetic_stanza)
+
+def clean_poem_title(poem_title):
+	poem_title = str.lower(poem_title)
+	poem_title = re.sub(' ','_',poem_title)
+	poem_title = re.sub('[^0-9a-zþæð\_]', '', poem_title)
+	return(poem_title)
 
 
-#poem_url='http://aspr.oepoetry.ca/a32.2.html'
+# local directory to deposit poem files
+local_corpus_directory = '/Users/ChadMorgan/Documents/old_english/OldEnglishPoetryCorpus/scraped_corpus'
+
+# get main list of poems housed in Labyrinth Library at Georgetown University
 poemdirectory_url = 'http://people.ucalgary.ca/~mmcgilli/OEPoetry/oepoems.htm'
-
 page = requests.get(poemdirectory_url)
 soup = BeautifulSoup(page.content, 'html.parser')
-
-print(soup.prettify())
-
 poem_links = soup.findAll("a", { "class" : "labyrinth" }, href=True)
 
-poem_ref = poem_links[0]['href']
-poem_page = requests.get(poem_ref)
-poem_soup = BeautifulSoup(poem_page.content, 'html.parser')
+for i in range(len(poem_links)):
+	# get link to poem page
+	poem_ref = poem_links[i]['href']
+	poem_page = requests.get(poem_ref)
+	poem_soup = BeautifulSoup(poem_page.content, 'html.parser')
+	# get link to poem text page
+	poem_text_page = requests.get(poem_soup.a.get_text())
+	poem_text_soup = BeautifulSoup(poem_text_page.content, 'html.parser')
+	# extract dd html tags for the actual poem
+	poem_text_raw = poem_text_soup.findAll("dd")
+	poem_title = poem_text_soup.findAll('title')[0].get_text()
+	poem_title = clean_poem_title(poem_title)
+	print(str(i)+": "+poem_title+'\n')
+	# clean and concatenate stanzas
+	for j in range(len(poem_text_raw)):
+		stanza = poem_text_raw[j].get_text()
+		stanza = clean_poetic_stanza(stanza)
+		if j==0:
+			full_poem_text = stanza
+		else:
+			full_poem_text += '\n' + stanza
+	# create local file and write poem to it
+	local_poem_file = local_corpus_directory+'/'+poem_title+'.txt'
+	os.system('touch '+local_poem_file)
+	f = open(local_poem_file, 'w')
+	f.write(full_poem_text)  
+	f.close()
 
-print(poem_soup.prettify())
 
-poem_text_page = requests.get(poem_soup.a.get_text())
-poem_text_soup = BeautifulSoup(poem_text_page.content, 'html.parser')
-print(poem_text_soup.prettify())
-
-poem_text_raw = poem_text_soup.findAll("dd")
-
-for stanza in poem_text_raw:
-	stanza = stanza.get_text()
-	stanza = clean_poetric_line(stanza)
